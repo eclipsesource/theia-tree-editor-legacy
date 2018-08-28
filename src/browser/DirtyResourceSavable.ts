a16ef93f8101b177dd03445f18e21cd4dd2a6313
Ahmet Tanakol <ahmettanakol@gmail.com>
2018-08-28 18:02:38 +0200

    display error message, dirty indicator and title icon

---
+++ src/browser/DirtyResourceSavable.ts
@@ -0,0 +1,34 @@
+import { ResourceSaveable } from './ResourceSaveable';
+import { unmanaged } from 'inversify';
+import { WidgetManager} from "@theia/core/lib/browser";
+import { MessageService, Resource } from '@theia/core/lib/common';
+import { DIRTY_CLASS, TreeEditorWidget } from './theia-tree-editor-widget';
+import * as _ from 'lodash';
+
+export class DirtyResourceSavable extends ResourceSaveable {
+  private _widgetManager: WidgetManager;
+  private _messageService: MessageService;
+
+  constructor(
+    resource: Resource,
+    getData: () => any,
+    @unmanaged() widgetManager: WidgetManager,
+    @unmanaged() messageService: MessageService
+  ) {
+    super(resource, getData);
+
+    this._widgetManager = widgetManager;
+    this._messageService = messageService;
+  }
+
+  onSaveSuccess = () => {
+    const widget = _.head(this._widgetManager.getWidgets('theia-tree-editor')) as TreeEditorWidget;
+    const dirtyClass = ` ${DIRTY_CLASS}`;
+    widget.title.className = widget.title.className.replace(dirtyClass, '');
+
+  }
+
+  onSaveFailure = () => {
+    this._messageService.error('Cannot Save: Undefined Resource');
+  }
+}
--- src/browser/index.ts
+++ src/browser/index.ts
@@ -2,3 +2,4 @@ export * from './theia-tree-editor-widget';
 export * from './tree-editor-utils';
 export * from './theia-tree-editor-contribution';
 export * from './ResourceSaveable';
+export * from './DirtyResourceSavable';
